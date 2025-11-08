70ff48c8f4b841397fa5e3b5a39686a52b5e5fbb
Isaac & Meja <isaackiarie29@gmail.com>
2025-11-08 13:23:26 +0300

    Modified

--- SACCO_Member_Wallet.py
+++ SACCO_Member_Wallet.py
@@ -5,14 +5,15 @@ Members = {}
 
 def register(name,balance):
     Members[name] = balance
-    print(f"Dear, {name} your balance is {balance}")
+    print(f"\nDear, {name} your balance is {balance}")
 
 def withdrawals(name, amount):
     if name in Members:
-        Members[name] -= amount
-        print(f"Successfully withdrawn {amount} from account. New Balance: KES {Members[name]}")
-    else:
-        print("Sorry Member Not Found!")
+        if Members[name] >= amount:
+            Members[name] -= amount
+            print(f"Successfully withdrawn {amount} from account. New Balance: KES {Members[name]}")
+        else:
+            print("Insufficient funds!")
 
 def deposit(name, amount):
     if name in Members:
@@ -25,7 +26,24 @@ def balance_check(name):
     if name in Members:
         print(f"You Balance is {Members[name]}")        
 
-register("Isaac", 4000)
-withdrawals("Isaac", 200)
-deposit("Isaac", 300)
-balance_check("Isaac")
+def loan(name, amount):
+    if name in Members:
+        Members[name] -= amount
+        print(f"You have successfully got the a loan of Kes {amount}")
+        print("Should be paid in 3 Months!")
+    else:
+        print("You are Not a Member!")
+
+
+def get_registration_details():
+    name = input("Enter Name: ")
+    depositing_amount = int(input("Enter the Deposit Amount: "))
+    return name, depositing_amount
+
+register_details = get_registration_details()
+
+
+register(register_details[0], register_details[1])
+withdrawals(register_details[0], 200)
+deposit(register_details[0], 300)
+balance_check(register_details[0])
